7ae551f4a7d4e098ccd9c84f2ea91e5059faf7e6
Parul Goyal <parul.goyal@generatecapital.com>
2021-12-12 23:51:39 -0800

    Adding query code and modifying schema code

--- schema.sql
+++ schema.sql
@@ -54,12 +54,11 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 -- Creating table for titles csv
 CREATE TABLE titles (
   	emp_no INT NOT NULL,
-  	title VARCHAR NOT NULL,
+  	title VARCHAR (40) NOT NULL,
   	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 -- Query for confirmation
-SELECT * FROM departments;
+SELECT * FROM titles;
